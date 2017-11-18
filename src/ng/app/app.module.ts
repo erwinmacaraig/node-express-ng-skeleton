8b8dea6a2f7cafad22a00fc4a46596205bb72bb8
Erwin Macaraig <erwin.macaraig@gmail.com>
2017-11-18 12:12:51 +0800

    Examples on Modules

--- src/ng/app/app.module.ts
+++ src/ng/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
 import { AppRoutingModule } from './app-routing.module';
-import { SampleComponent } from './sample/sample.component';
+import { SharedModule } from './shared/shared.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { AuthModule } from './auth/auth.module'
+import { CoreModule } from './core/core.module';
+
 @NgModule({
   declarations: [
-    AppComponent,
-    SampleComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpModule,
+    AppRoutingModule,
+    SharedModule,
+    ShoppingListModule,
+    AuthModule,
+    CoreModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
